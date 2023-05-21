4f1ea1536edc4fd83a85d0203b7a2d504791abbd
Abdellatif Agnaou <abdellatifagn@gmail.com>
2023-05-21 09:10:17 +0100

    Update streamlit_app.py
    Adjusted Emos

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title("Healthy Dinner !")
 
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🍞 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑 Avocado Toast')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
