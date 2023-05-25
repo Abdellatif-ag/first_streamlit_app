4a4df8c451aed8f15e2dac23c5b4027e81187a8b
Abdellatif Agnaou <abdellatifagn@gmail.com>
2023-05-25 07:10:36 +0100

    Update streamlit_app.py
    Added SF Insert Fruit Function

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,11 @@ def get_fruit_load_list():
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
     
+def insert_row_snowflake(fruit):
+  with my_cnx.cursor() as my_cur
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+    return 'Thanks for adding ' + fruit
+    
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -58,5 +63,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.text('Thanks for adding ' + add_my_fruit)
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_fun = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_fun)
+
